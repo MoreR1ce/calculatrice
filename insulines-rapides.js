e4c3359114e7c7a9b383776315f80b87125fb31a
MoreR1ce <morrisgaron66@gmail.com>
2025-02-05 10:41:53 -0500

    Update insulines-rapides.js

--- insulines-rapides.js
+++ insulines-rapides.js
@@ -1,1 +1,12 @@
 
+    FunctionC();
+    const buttonC = document.querySelector("#button_C");
+    const answerC = document.querySelector("#answer_C");
+
+function FunctionC() {
+    var dose = document.querySelector("#dose-départ-insuline-rapide").value;
+    var insulinerd = document.querySelector("#insuline-rapide-départ").value;
+    var insulinerf = document.querySelector("#insuline-rapide-finale").value;
+    let yolo = insulinerd + " " + dose + "u ----» " + insulinerf + " " + dose + "u";
+    document.getElementById("answer_C").textContent = yolo;
+}
